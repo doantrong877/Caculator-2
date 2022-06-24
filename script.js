1ca80863856b78a948bf0b3dbc16a35201c3bc6b
Trong Doan <doantrong8777@gmail.com>
2022-06-24 16:09:50 -0700

    finished calculation

--- script.js
+++ script.js
@@ -1,21 +1,58 @@
 var displayDiv = document.querySelector("#display");
 var arr = [];
-var arrIndex = 0;
+var result = [];
 function press(num) {
-    arr[arrIndex] += num;
-    console.log(arr[0]);
+
+    if(result.length != 0){
+        if(isNaN(result[result.length-1])){
+            displayDiv.innerText = '0';
+        };
+    };
+        
+    arr.push(num);
+    displayDiv.innerText = arr.join("");
+    result.push(num);
+    console.log(arr.join(""));
     
 }
 
 function setOP(sign) {
 
+    switch (sign) {
+        case '/':          
+            result.push(sign);
+            console.log(result);
+            break;
+        case '*':
+            result.push(sign);
+            console.log(result);
+            break;
+        case '-':
+            result.push(sign);
+            console.log(result);
+            break;
+        case '+':
+            result.push(sign);
+            console.log(result);
+            break;   
+    }
   
 }
 
 function calculate() {
-   
+    try{
+        displayDiv.innerText = eval(result.join("")).toString();
+    } catch {
+        console.log("there was an error in the calculation");
+    }
+    
+    arr = [];
+    result = [];
 }
 
 function clr() {
+    displayDiv.innerText = '0';
+    arr = [];
+    result = [];
     
 }
