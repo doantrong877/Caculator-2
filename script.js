2310dd351a775787824cccbcd90c0b004198957e
Trong Doan <doantrong8777@gmail.com>
2022-06-24 15:37:11 -0700

    bug fix html

---
+++ script.js
@@ -0,0 +1,21 @@
+var displayDiv = document.querySelector("#display");
+var arr = [];
+var arrIndex = 0;
+function press(num) {
+    arr[arrIndex] += num;
+    console.log(arr[0]);
+    
+}
+
+function setOP(sign) {
+
+  
+}
+
+function calculate() {
+   
+}
+
+function clr() {
+    
+}
